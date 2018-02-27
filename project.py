71108504d84dc179265411656d3e2ab7ac59f86e
nobYsDarling <till.hildebrandt@gmail.com>
2018-02-27 09:40:02 +0100

    Added initial compairision scripts

--- project.py
+++ project.py
@@ -4,6 +4,10 @@ import re
 import numpy
 from PIL import Image
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+import math
 
 PATH_ORIGINAL = 'images/thumbnails/'
 PATH_BLURRED_03 = 'images/blurred_03/'
@@ -17,13 +21,32 @@ PATH_MOSAIC_15 = 'images/mosaic_15/'
 PATH_MOSAIC_20 = 'images/mosaic_20/'
 PATH_MOSAIC_25 = 'images/mosaic_25/'
 
+TRAIN_RATIO = 0.2
+
+
+def image_path_to_array(path):
+    return numpy.array(Image.open(path)).flatten().tolist()
+
+
 if __name__ == '__main__':
-    # img = Image.open('images/blurred_03/00002_930831_fa.ppm')
-    # print(numpy.array(img))
+    X = y = []
+
+    for filename in os.listdir(PATH_BLURRED_06):
+        X.append(image_path_to_array(os.path.join(PATH_ORIGINAL, filename)))
 
     for filename in os.listdir(PATH_ORIGINAL):
-        if filename.endswith(".ppm"):
-            print(os.path.join(PATH_ORIGINAL, filename))
-            continue
-        else:
-            continue
+        y.append(image_path_to_array(os.path.join(PATH_ORIGINAL, filename)))
+
+    print 'Dataset loaded...'
+
+    train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=TRAIN_RATIO)
+
+    # classifier =
+    classifier = MLPRegressor(solver='lbfgs', alpha=1e-5,
+                              hidden_layer_sizes=(5, 2), random_state=1)
+
+    print 'Start training...'
+    classifier.fit(train_x, train_y)
+    print 'Training finished...'
+
+    print(classifier.score(test_x, test_y))
